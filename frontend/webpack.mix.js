87feb7cef5c4785c17a4196cb1fe04f60c67ff05
HamzaouiSatoripop <42346955+HamzaouiSatoripop@users.noreply.github.com>
2018-12-07 05:53:03 +0100

    add new file front

---
+++ frontend/webpack.mix.js
@@ -0,0 +1,46 @@
+let mix = require('laravel-mix');
+let exec = require('child_process').exec;
+let path = require('path');
+
+/*
+ |--------------------------------------------------------------------------
+ | Mix Asset Management
+ |--------------------------------------------------------------------------
+ |
+ | Mix provides a clean, fluent API for defining some Webpack build steps
+ | for your Laravel application. By default, we are compiling the Sass
+ | file for the application as well as bundling up all the JS files.
+ |
+ */
+
+mix
+    .copyDirectory('resources/assets/fonts', 'public/fonts')
+    .sass('resources/assets/sass/app.scss', 'public/css')
+    .js('resources/assets/js/app.js', 'public/js')
+    .copy('node_modules/sweetalert/dist/sweetalert.min.js', 'public/js/sweetalert.min.js')
+    .copy('resources/assets/img', 'public/img')
+    .sass('resources/assets/sass/app-rtl.scss', 'public/css')
+    .then(() => {
+        exec('node_modules/rtlcss/bin/rtlcss.js public/css/app-rtl.css ./public/css/app-rtl.css');
+    })
+    .version(['public/css/app-rtl.css'])
+    .webpackConfig({
+        resolve: {
+            modules: [
+                path.resolve(__dirname, 'vendor/laravel/spark-aurelius/resources/assets/js'),
+                'node_modules'
+            ],
+            alias: {
+                'vue$': 'vue/dist/vue.js'
+            }
+        }
+    })
+    .options({
+        processCssUrls: false
+    })
+    .babelConfig({
+        presets: [
+                'env'
+             ], 
+        plugins: ["transform-vue-jsx"]
+   });
